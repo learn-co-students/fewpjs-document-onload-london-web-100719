b3302c3d46a3b4998e3bb7678c4dce71569b658c
flow1981 <contact.f.stagliano@gmail.com>
2019-11-18 18:20:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const text = document.querySelector("#text")
+text.innerText = "JavaScript is so cool. It lets me add text to my page programmatically."
+
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.querySelector("#text")
+    text.innerHTML = "This is really cool!"
+  });
