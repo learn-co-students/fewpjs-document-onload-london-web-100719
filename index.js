715f0eb003ab81f0669ab0687d2ffee4fe86e415
github username <github email address>
2019-11-19 14:19:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    updateDOM();
+  });
+
+function updateDOM(){
+  let text = document.querySelector("#text");
+  text.textContent = 'This is really cool!';
+}
